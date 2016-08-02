d1c7e6afccf931f823225295c6f6ea4081721977
carolinux <carolinegr@gmail.com>
2016-08-02 10:47:56 +0200

    Now showing the rating

--- web.py
+++ web.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
+
 app = Flask(__name__)
 
 
@@ -13,12 +16,13 @@ def hospital_search():
     if request.method == "GET":
         return render_template('search.html')
     else:
-        hospital_name = request.form.get('hospital_name')
+        hospital_name_with_id = request.form.get('hospital_name')
         # remove the (number) at the end
-        last_idx = hospital_name.rfind("(")
-        hospital_name = hospital_name[:last_idx].strip()
-        # TODO: placeholder - do something with the hospital name
-        return "You searched for {}".format(hospital_name)
+        last_idx = hospital_name_with_id.rfind("(")
+        hospital_name = hospital_name_with_id[:last_idx].strip()
+        provider_id = int(hospital_name_with_id[last_idx+1: -1])
+        rating = overall_ratings.ix[provider_id]
+        return "Overall Hospital Rating  for {}: {}".format(hospital_name_with_id, rating)
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
@@ -28,6 +32,13 @@ def autocomplete():
 
 hospitals = pd.read_csv("data/HCAHPS - Hospital.csv")
 unique_names = get_unique_hospital_names(hospitals)
+survey_fn = os.path.join('data', 'HCAHPS - Hospital.csv')
+survey = pd.read_csv(survey_fn, engine="python")
+overall_rating_question = 'Overall hospital rating - star rating'
+rating_col = "Patient Survey Star Rating"
+question_col = 'HCAHPS Question'
+survey_overall = survey[survey[question_col]==overall_rating_question]
+overall_ratings = survey_overall.groupby("Provider ID")[rating_col].min()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
