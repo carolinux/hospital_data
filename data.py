4f7f999db7d02aa8f766974ce2b88e2f025c4d0a
carolinux <carolinegr@gmail.com>
2016-12-09 15:27:26 +0100

    Refactoring the code

---
+++ data.py
@@ -0,0 +1,121 @@
+__author__ = 'carolinux'
+
+import json
+import os
+import requests
+
+import pandas as pd
+
+def to_capitals(pythoncase_str):
+    return pythoncase_str.replace("_", " ").capitalize()
+
+
+class HospitalData(object):
+
+    score_forms = {"Readmissions_Reduction": "kac9-a9fp", # Hospital Readmissions Reduction Program
+    "Complications": "r32h-32z5", # Complications - Hospital,
+    "Healthcare_Associated_Infections": "ppaw-hhm5", # Healthcare Associated Infections - Hospital
+    "Timely and Effective Care": "3z8n-wcgr", # Timely and Effective Care - Hospital
+    "Medicare Spending Per Patient": "9i85-rqbi" # Medicare Hospital Spending Per Patient - Hospital
+    }
+    get_less_data = True
+    HCAPS_ID = "dgck-syfz"
+    HOSPITAL_DATA_ID = "rbry-mqwu"
+
+    overall_rating_question = 'Overall hospital rating - star rating'
+    rating_col = "patient_survey_star_rating" # before it was like Patient Survey Star Rating
+    question_col = 'hcahps_question'
+    unique_names = None
+
+    def __init__(self, get_less_data=False):
+
+        self.score_data = {}
+        for score_type, form_id in self.score_forms.items():
+            data = self.get_hospital_data_from_web(form_id, do_filter=get_less_data)
+            data = data.set_index("provider_id", drop=False)
+            self.score_data[score_type] = data
+
+        print("getting hospital data")
+        self.hospitals = self.get_hospital_data_from_web(self.HOSPITAL_DATA_ID, do_filter=get_less_data)
+        self.unique_names = self.get_unique_hospital_names()
+        print ("getting survey data")
+        self.survey = self.get_hospital_data_from_web(self.HCAPS_ID, do_filter=get_less_data)
+
+
+        print ("getting ratings")
+        self.survey_overall = self.survey[self.survey[self.question_col]==self.overall_rating_question] # select only
+        # the
+        # question of 'overall hospital rating'
+        # find the rating per provider id
+        self.overall_ratings = self.survey_overall.groupby("provider_id")[self.rating_col].min()
+        print ("loaded everything")
+
+    def get_all_ratings_for_provider_id(self, provider_id):
+        """Get a dataframe with the ratings for a provider id """
+
+        survey_of_hospital = self.survey[self.survey["provider_id"] == provider_id][[self.question_col,
+                                                                                     self.rating_col]]
+        survey_of_hospital = survey_of_hospital[survey_of_hospital[self.rating_col].isin(
+            ['1', '2', '3', '4', '5'])]
+        survey_of_hospital[self.question_col] = survey_of_hospital[self.question_col].apply(lambda x: x.split(" - "
+                                                                                                            "star")[0])
+        survey_of_hospital.columns = [self.question_col, self.rating_col+" ( 1 to 5, higher is better)"]
+
+        regular_questions = survey_of_hospital[(survey_of_hospital[self.question_col]!=self.overall_rating_question) &
+                                             (survey_of_hospital[self.question_col]!='summary_star_rating') ]
+        summary_question = survey_of_hospital[survey_of_hospital[self.question_col]==self.overall_rating_question]
+        summary_question[self.question_col] = summary_question[self.question_col].apply(lambda x: x.upper())
+        ratings = pd.concat([regular_questions, summary_question])
+        ratings.columns = map(to_capitals, ratings.columns)
+        return ratings
+
+    def get_hospital_data(self, provider_id):
+        return self.hospitals[self.hospitals["provider_id"] == provider_id].iloc[0] # the data for this hospital
+
+    def get_hospital_data_from_web(self, data_id, do_filter=False):
+        """Gets the data from the json API and returns it as a dataframe"""
+        resource_url = "https://data.medicare.gov/resource/{}.json".format(data_id)
+        token = "bjp8KrRvAPtuf809u1UXnI0Z8" # test token
+        #token = "UKABSmwwYK2lyRyiKYHDn2V9c" # adelard's token
+        print ("querying for hospital data")
+        if do_filter:
+            # get only some data
+            resource_url +="?$where=starts_with(hospital_name,'SOUTH')"
+        resp = requests.get(resource_url, headers={"X-App-Token": token}, verify=False)
+        results = json.loads(resp.text)
+        df = pd.DataFrame.from_records(results)
+        return df
+
+
+    def get_unique_hospital_names(self):
+        """Get unique hospital names (together with provider id """
+        # add provider name to the hospital name
+        if self.unique_names is None:
+            unique_names = self.hospitals.apply(lambda x: x["hospital_name"]+" ("+str(x["provider_id"])+")" ,
+                                         axis=1).unique()
+            self.unique_names = sorted(unique_names)
+
+        return self.unique_names
+
+    def get_measure_scores(self, provider_id):
+        """
+         Get all the score data
+        """
+        measures = []
+        for form_name, data in self.score_data.items():
+            if "Readmissions" in form_name:
+                continue
+            try:
+                data = data.ix[provider_id]
+                if isinstance(data, pd.Series):
+                    measures.append((form_name + ": " + data["measure_name"], data["score"]))
+                else:
+                    for _, row in data.iterrows():
+                        measures.append((form_name + ":" + row["measure_name"], row["score"]))
+            except KeyError:
+                print ("Provider id {} has not data in {}".format(provider_id, form_name))
+        return measures
+
+
+
+
--- web.py
+++ web.py
@@ -1,44 +1,20 @@
-import json
-import os
-import requests
+import sys
 
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 
+import data
+
 app = Flask(__name__) # this creates an instance of the flask app
 
-
-def get_unique_hospital_names(hospitals_df): # this is just a regular function
-    # add provider name to the hospital name
-    unique_names = hospitals_df.apply(lambda x: x["hospital_name"]+" ("+str(x["provider_id"])+")" , axis=1).unique()
-    return sorted(unique_names)
-
-def to_capitals(pythoncase_str):
-    return pythoncase_str.replace("_"," ").capitalize()
-
-
-def get_measure_scores(provider_id):
-    measures = []
-    for form_name, data in score_data.items():
-        if "Readmissions" in form_name:
-            continue
-        try:
-            data = data.ix[provider_id]
-            if isinstance(data, pd.Series):
-                measures.append((form_name + ": " + data["measure_name"], data["score"]))
-            else:
-                for _, row in data.iterrows():
-                    measures.append((form_name + ":" + row["measure_name"], row["score"]))
-        except KeyError:
-            print ("Provider id {} has not data in {}".format(provider_id, form_name))
-    return measures
-
+cache = {}
 
 @app.route('/search', methods=['GET', 'POST']) # when I go to the url/search, return whatever this function returns
 def hospital_search():
     if request.method == "GET":
         return render_template('search.html') # this renders the form with the autocomplete
     else:
+        d = cache["data"]
         # here is what happens when the search form is submitted
         # this is where we get the rating
         hospital_name_with_id = request.form.get('hospital_name')
@@ -46,81 +22,30 @@ def hospital_search():
         last_idx = hospital_name_with_id.rfind("(")
         hospital_name = hospital_name_with_id[:last_idx].strip()
         provider_id = str(hospital_name_with_id[last_idx+1: -1])
-        rating = overall_ratings.ix[provider_id]
-        summary = "<u>Overall Hospital Rating</u>  for {}: {}".format(hospital_name_with_id, rating)
-        survey_of_hospital = survey[survey["provider_id"] == provider_id][[question_col, rating_col]]
-        survey_of_hospital = survey_of_hospital[survey_of_hospital[rating_col].isin(['1','2','3','4','5'])]
-        survey_of_hospital[question_col] = survey_of_hospital[question_col].apply(lambda x: x.split(" - star")[0])
-        survey_of_hospital.columns = [question_col, rating_col+" ( 1 to 5, higher is better)"]
 
-        regular_questions = survey_of_hospital[(survey_of_hospital[question_col]!=overall_rating_question) &
-                                             (survey_of_hospital[question_col]!='summary_star_rating') ]
-        summary_question = survey_of_hospital[survey_of_hospital[question_col]==overall_rating_question]
-        # handle measures (TODO: abstract to function)
-        measures = get_measure_scores(provider_id)
+        measures = d.get_measure_scores(provider_id)
+        ratings = d.get_all_ratings_for_provider_id(provider_id)
 
-        summary_question[question_col] = summary_question[question_col].apply(lambda x:x.upper())
-        ratings = pd.concat([regular_questions, summary_question])
-        ratings.columns = map(to_capitals, ratings.columns)
+        hospital_data = d.get_hospital_data(provider_id)
+        longitude = hospital_data["location"]["coordinates"][0]
+        latitude = hospital_data["location"]["coordinates"][1]
 
-        d = hospitals[hospitals["provider_id"] == provider_id].iloc[0] # the data for this hospital
-        longitude = d["location"]["coordinates"][0]
-        latitude = d["location"]["coordinates"][1]
-
-        return render_template("map.html", summary_data=d, 
+        return render_template("map.html", summary_data=hospital_data,
                 ratings=ratings, longitude=longitude,
                 latitude=latitude, measures=measures)
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     search_q = request.args.get('q').lower()
-    results = list(filter(lambda x: x.lower().startswith(search_q), unique_names))
+    results = list(filter(lambda x: x.lower().startswith(search_q), cache["data"].get_unique_hospital_names()))
     return jsonify(matching_results=results)
 
-def get_hospital_data_from_web(data_id, do_filter=False):
-    resource_url = "https://data.medicare.gov/resource/{}.json".format(data_id)
-    token = "bjp8KrRvAPtuf809u1UXnI0Z8" # test token
-    #token = "UKABSmwwYK2lyRyiKYHDn2V9c" # adelard's token
-    print ("querying for hospital data")
-    if do_filter:
-        # get only some data
-        resource_url +="?$where=starts_with(hospital_name,'SOUTH')"
-    resp = requests.get(resource_url, headers={"X-App-Token": token})
-    results = json.loads(resp.text)
-    df = pd.DataFrame.from_records(results)
-    return df
-
-score_forms = {"Readmissions_Reduction": "kac9-a9fp", # Hospital Readmissions Reduction Program
-"Complications": "r32h-32z5", # Complications - Hospital,
-"Healthcare_Associated_Infections": "ppaw-hhm5", # Healthcare Associated Infections - Hospital
-"Timely and Effective Care": "3z8n-wcgr", # Timely and Effective Care - Hospital
-"Medicare Spending Per Patient": "9i85-rqbi" # Medicare Hospital Spending Per Patient - Hospital
-}
-get_less_data = True
-HCAPS_ID = "dgck-syfz"
-HOSPITAL_DATA_ID = "rbry-mqwu"
-
-score_data = {}
-for score_type, form_id in score_forms.items():
-    data = get_hospital_data_from_web(form_id, do_filter=get_less_data)
-    data = data.set_index("provider_id", drop=False)
-    score_data[score_type] = data
-       
-
-print("getting hospital data")
-hospitals = get_hospital_data_from_web(HOSPITAL_DATA_ID, do_filter=get_less_data)
-unique_names = get_unique_hospital_names(hospitals)
-print ("getting survey data")
-survey = get_hospital_data_from_web(HCAPS_ID, do_filter=get_less_data)
-overall_rating_question = 'Overall hospital rating - star rating'
-rating_col = "patient_survey_star_rating" # before it was like Patient Survey Star Rating
-question_col = 'hcahps_question'
-print ("getting ratings")
-survey_overall = survey[survey[question_col]==overall_rating_question] # select only the question of 'overall hospital rating'
-# find the rating per provider id
-overall_ratings = survey_overall.groupby("provider_id")[rating_col].min()
-print ("loaded everything")
 
 if __name__ == '__main__':
     print ("starting webserver")
+    if len(sys.argv)==2:
+        is_test = sys.argv[1] == 'test'
+    else:
+        is_test = False
+    cache["data"] = data.HospitalData(get_less_data=is_test)
     app.run(port=5000, debug=True, processes=1)
